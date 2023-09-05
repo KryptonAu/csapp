3a7ddb90a276bbee041f20506c368beddb8eec71
KryptonAu <1466129513@qq.com>
2023-09-05 16:40:55 +0800

    attack_lab_rtarget_9.5

---
+++ attack_lab/rtarget_files/phase_2.s
@@ -0,0 +1,82 @@
+mov    $0x909078fb,%eax
+ret    
+lea    -0x3c3876b8(%rdi),%eax
+ret    
+lea    -0x6fa78caf(%rdi),%eax
+ret    
+movl   $0xc7c78948,(%rdi)
+ret    
+movl   $0x9258c254,(%rdi)
+ret    
+movl   $0xc78d4863,(%rdi)
+ret    
+movl   $0x90c78948,(%rdi)
+ret    
+mov    $0xc3905829,%eax
+ret    
+lea    (%rdi,%rsi,1),%rax
+ret    
+mov    $0x90c2895c,%eax
+ret    
+movl   $0x9090d199,(%rdi)
+ret    
+lea    -0x36873177(%rdi),%eax
+ret    
+lea    -0x24df2e73(%rdi),%eax
+ret    
+mov    $0xc048d189,%eax
+ret    
+movl   $0xc084d181,(%rdi)
+ret    
+lea    -0x1f76b7bf(%rdi),%eax
+ret    
+movl   $0xc908c288,(%rdi)
+ret    
+lea    -0x6f6f3177(%rdi),%eax
+ret    
+mov    $0xc1e08948,%eax
+ret    
+lea    -0x36ff3d77(%rdi),%eax
+ret    
+lea    -0x3fc73177(%rdi),%eax
+ret    
+movl   $0xdb08ce81,(%rdi)
+ret    
+mov    $0xc938d189,%eax
+ret    
+lea    -0x3c1f7638(%rdi),%eax
+ret    
+lea    -0x3f7b3d77(%rdi),%eax
+ret    
+lea    -0x381f76b8(%rdi),%eax
+ret    
+mov    $0xd208d199,%eax
+ret    
+mov    $0xc9c4c289,%eax
+ret    
+movl   $0x91e08948,(%rdi)
+ret    
+lea    -0x3c6d3177(%rdi),%eax
+ret    
+mov    $0xdb08d189,%eax
+ret    
+movl   $0xc391d189,(%rdi)
+ret    
+movl   $0xd238c281,(%rdi)
+ret    
+movl   $0xc908ce09,(%rdi)
+ret    
+lea    -0x6f1f76f8(%rdi),%eax
+ret    
+lea    0x3cc7c289(%rdi),%eax
+ret    
+mov    $0xc020ce88,%eax
+ret    
+movl   $0xc2e08948,(%rdi)
+ret    
+lea    -0x2d9f3d77(%rdi),%eax
+ret    
+mov    $0xd220ce8d,%eax
+ret    
+movl   $0x90e08948,(%rdi)
+ret    
